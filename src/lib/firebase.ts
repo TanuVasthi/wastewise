e4823e5e2386c67438ed5fc13dbec261cc69fa62
TanuVasthi <vasthitanusree@gmail.com>
2025-07-06 21:22:54 +0530

    dashboard hanges

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyA7kIivXXggZZH4HvG-4TU06POySNEFSYM",
+  authDomain: "wastewise-analytics-1z9zt.firebaseapp.com",
+  projectId: "wastewise-analytics-1z9zt",
+  storageBucket: "wastewise-analytics-1z9zt.firebasestorage.app",
+  messagingSenderId: "508279724489",
+  appId: "1:508279724489:web:ba8f705beabb3ebdab3018"
 };
-
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
